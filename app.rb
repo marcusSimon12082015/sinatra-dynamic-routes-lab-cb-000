02764945c8543530faf407a7a1393a9371918ec2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-21 19:55:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,9 @@ class App < Sinatra::Base
   get '/reversename/:name' do
       "#{params[:name].reverse}"
   end
+
+  get '/square/:number' do
+    @number = params[:number].to_i * 2 
+    "#{@number}"
+  end 
 end
