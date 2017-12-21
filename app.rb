97239e44797b5d193dacc22173516fa9ebff421a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-21 20:19:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    binding.pry
+    #binding.pry
     erb :phrase
   end
 end
