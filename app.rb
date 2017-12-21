73617b74511526c03e47099c8a5bc2544309156e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-21 20:25:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     @return_string
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do 
-    
-  end 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
 end
